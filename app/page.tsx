da8bc8bf8db559f4ecc21eea7a74dc0aeb2958ed
DahuiSheng <71542476+DahuiSheng@users.noreply.github.com>
2023-03-28 12:46:11 +0900

    Add openai

--- app/page.tsx
+++ app/page.tsx
@@ -10,3 +10,30 @@ export default function Home() {
     </main>
   )
 }
+
+
+/**
+ * ChatGPT API（gpt-3.5-turbo）を Node.js から使ってみる（公式リファレンスで参照した情報も記載）
+ * https://qiita.com/youtoy/items/b3e2e462029bf40f8a1a
+ * 　このコードをベースに。
+ * $ npm i openai
+ *
+ * https://platform.openai.com/docs/api-reference/chat/create?lang=node.js
+ *  これ、Example requestの右上にcurl, python, node.jsの選択がある！これは気が付かんわー
+ *
+ * https://openai.com/product
+ *
+ * https://platform.openai.com/account/api-keys
+ *
+ * https://twitter.com/yanosen_jp/status/1631074488363384832
+ *
+ * https://qiita.com/youtoy/items/84384ad7a742ea1ce8f5
+ *
+ * https://platform.openai.com/docs/libraries/node-js-library
+ *
+ * https://platform.openai.com/docs/api-reference/authentication
+ *
+ * https://platform.openai.com/docs/api-reference/searches
+ */
+
+
